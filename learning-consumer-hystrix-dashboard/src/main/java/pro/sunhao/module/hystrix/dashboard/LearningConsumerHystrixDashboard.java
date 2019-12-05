16da254b0c04312bddbb54255ddea1fb6f4a774e
隼耗 <sunhao1109@outlook.com>
2019-12-05 12:02:25 +0800

    加入 hystrixDashboard

---
+++ learning-consumer-hystrix-dashboard/src/main/java/pro/sunhao/module/hystrix/dashboard/LearningConsumerHystrixDashboard.java
@@ -0,0 +1,13 @@
+package pro.sunhao.module.hystrix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboard
+@SpringBootApplication
+public class LearningConsumerHystrixDashboard {
+    public static void main(String[] args) {
+        SpringApplication.run(LearningConsumerHystrixDashboard.class, args);
+    }
+}
